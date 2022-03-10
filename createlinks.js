7ecf50fbaa5d05110c32ba709a2673c84e8e9a94
Sadasiva20 <99778144+Sadasiva20@users.noreply.github.com>
2022-03-10 10:43:23 -0500

    Add files via upload

---
+++ createlinks.js
@@ -0,0 +1,36 @@
+export default function CreateLinks(){
+
+return (
+
+<>
+<h1>
+     Website Information
+</h1>
+<div className="text-center">
+    <label>
+        Title
+    </label>
+    <>       </>
+    <input type="text"></input>
+   
+    </div>
+    <div className = "text-center">
+    <label>
+         link
+    </label>
+    <>      </>
+    <input type="text"></input> <> </>  <input type="submit" value="Add link" /> 
+    
+    </div>
+    
+   
+
+    
+        
+     
+
+</>
+
+)
+
+}
