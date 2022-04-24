a7d67ebd9f35fd5a49495c209c9cf630967ef989
Kuhgelfische <chopkins95@live.com>
2022-04-23 17:10:10 -0700

    Add username support

--- api/routes/accounts.js
+++ api/routes/accounts.js
@@ -10,10 +10,10 @@ router.use(express.json({}));
 
 router.post('/createAcct', (req, res) => {
   // Pull out the payload fields
-  const { email, password1, password2 } = req.body;
+  const { username, email, password1, password2 } = req.body;
 
   // Check empty fields
-  if (!email || !password1 || !password2) {
+  if (!username || !email || !password1 || !password2) {
     res.status(401).json({ ok: false, msg: "Please enter all fields" });
     return;
   }
@@ -23,9 +23,21 @@ router.post('/createAcct', (req, res) => {
     res.status(401).json({ ok: false, msg: "Passwords do not match" });
     return;
   }
-
+  // Usernames and emails will be lowercase from here on out.
+  var userN = username.toLowerCase();
+  var eMail = email.toLowerCase();
+  for(var i of accountStore) {
+    if(i.username === userN) {
+      res.status(401).json({ok: false, msg: "An account with that username already exists."})
+      return;
+    } else if(i.email === eMail) {
+      res.status(401).json({ok: false, msg: "An account with that email address already exists."})
+      return;
+    }
+  }
   accountStore.push({
-    email,
+    username: userN,
+    email: eMail,
     password: bcrypt.hashSync(password1, 5)
   });
 
@@ -38,16 +50,15 @@ router.post('/createAcct', (req, res) => {
 });
 
 router.post('/login', (req, res) => {
-  const {email, password} = req.body;
+  const {username, password} = req.body;
 
-  if(!email || !password) {
+  if(!username || !password) {
     res.status(401).json({ok: false, msg: "Please enter all fields"});
   }
   for(var i of accountStore) {
-
-    if(i.email && i.email === email) {
+    if(i.username && i.username === username.toLowerCase()) {
       if(bcrypt.compareSync(password, i.password)) {
-        const token = jwt.sign({ username: i.email }, 'secretvalue');
+        const token = jwt.sign({ username: i.username }, 'secretvalue');
         res.json({
           ok: true,
           data: {
